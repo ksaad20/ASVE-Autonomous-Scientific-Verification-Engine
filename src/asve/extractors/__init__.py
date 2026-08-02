9189b9ee0839675519a87f1284ef528c20e235f0
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-08-02 21:46:16 +0600

    Add __init__.py for ASVE data extractors

---
+++ src/asve/extractors/__init__.py
@@ -0,0 +1,6 @@
+"""ASVE data extractors."""
+
+from asve.extractors.base import BaseExtractor
+from asve.extractors.registry import ExtractorRegistry
+
+__all__ = ["BaseExtractor", "ExtractorRegistry"]
