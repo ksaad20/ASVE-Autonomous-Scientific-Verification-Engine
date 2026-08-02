7bd7b711d1448d734f040caf06262909732d93fd
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-08-02 22:35:55 +0600

    Add BaseParser to module exports

--- src/asve/parsers/base.py
+++ src/asve/parsers/base.py
@@ -62,7 +62,10 @@ class ArtifactParser(ABC):
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}()"
 
+BaseParser = Parser
 
 __all__ = [
-    "ArtifactParser",
+    "BaseParser",
+    "Parser",
+    "ArtifactParser"
 ]
